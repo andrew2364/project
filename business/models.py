1e5451f2934ccbcccfed6921d39e33c2f6cfbba7
andrew <tel10@yandex.ru>
2019-05-09 12:28:22 +0300

    добавили кармичность в бизнес название итог1

--- business/models.py
+++ business/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class Arkans(models.Model):
+
 	arkan_number = models.IntegerField(unique=True)
 	cel = models.TextField(max_length=1000)
 	osnova = models.TextField(max_length=1000)
@@ -10,9 +11,9 @@ class Arkans(models.Model):
 
 class BusinessForm(forms.Form):
 
-	name = forms.CharField(label='Ваше имя', max_length=50, required=False)
-	birth = forms.DateField(required = False, label='День рождения', widget=forms.SelectDateWidget(years=range(1950, 2020)))
-	business_name = forms.CharField(label='Название бизнеса', max_length=200)
+	name = forms.CharField(label='Ваше имя ', max_length=50, required=False)
+	birth = forms.DateField(required = False, label='День рождения ', widget=forms.SelectDateWidget(years=range(1930, 2020)))
+	business_name = forms.CharField(label='Название бизнеса ', max_length=200)
 
 
 
--- business/views.py
+++ business/views.py
@@ -1,151 +1,156 @@
 from django.shortcuts import render
 from business.models import BusinessForm, Arkans
-import string
-from math import *
 
 
-def hello(request):
-    return render(request, "index.html")
+def hello(request) :
+	return render(request, "index.html")
 
 
-def rezultat(request, name=None):
+def rezultat(request, name=None) :
+	if request.method == "POST" :
+		arkan_cel = {}
+		arkan_osnova = {}
+		arkan_jertva = {}
+		t = 0
+		osnova = 0
+		jertva = 0
+		karma_txt = ''
 
-    if request.method == "POST":
-        arkan_cel = {}
-        arkan_osnova = {}
-        arkan_jertva = {}
-        t = 0
-        osnova = 0
-        jertva = 0
-        karma_txt = ''
+		userform = BusinessForm(request.POST)
+		info = request.POST
+		business = info['business_name'].lower()  # lower case letters
+		birth_day = info['birth_day']
+		birth_month = info['birth_month']
+		birth_year = info['birth_year']
 
+		# dict with alphabet
 
-        userform = BusinessForm(request.POST)
-        info = request.POST
-        i = info.dict()
-        business = i['business_name'].lower() # lower case letters
-        birth_day = i['birth_day']
-        birth_month = i['birth_month']
-        birth_year = i['birth_year']
+		d = {
+			'а' : 1, 'б' : 2, 'в' : 3, 'г' : 4, 'д' : 5, 'е' : 6, 'ё' : 7, 'ж' : 8,
+			'з' : 9, 'и' : 1, 'й' : 2, 'к' : 3,	'л' : 4, 'м' : 5, 'н' : 6, 'о' : 7,
+			'п' : 8, 'р' : 9, 'с' : 1, 'т' : 2, 'у' : 3, 'ф' : 4, 'х' : 5, 'ц' : 6,
+			'ч' : 7, 'ш' : 8, 'щ' : 9, 'ъ' : 1, 'ы' : 2, 'ь' : 3, 'э' : 4, 'ю' : 5,
+			'я' : 6,
 
+			'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7, 'h' : 8,
+			'i' : 9, 'j' : 1, 'k' : 2, 'l' : 3, 'm' : 4, 'n' : 5, 'o' : 6, 'p' : 7,
+			'q' : 8, 'r' : 9, 's' : 1, 't' : 2, 'u' : 3, 'v' : 4, 'w' : 5, 'x' : 6,
+			'y' : 7, 'z' : 8,
 
-        # dict with alphabet
+			'1' : 1, '2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9
+		}
 
-        d = {
-            'а': 1, 'б': 2, 'в': 3, 'г': 4, 'д': 5, 'е': 6, 'ё': 7, 'ж': 8, 'з': 9, 'и': 1, 'й': 2, 'к': 3, 'л': 4,
-            'м': 5, 'н': 6, 'о': 7, 'п': 8, 'р': 9, 'с': 1, 'т': 2, 'у': 3, 'ф': 4, 'х': 5, 'ц': 6, 'ч': 7, 'ш': 8, 'щ': 9, 'ъ': 1,
-            'ы': 2, 'ь': 3, 'э': 4, 'ю': 5, 'я': 6, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
-        }
+		# Массив, содержащий согласные буквы
 
-        # Массив, содержащий согласные буквы
+		sogl = ['б', 'в', 'г', 'д', 'ж', 'з', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф',
+		        'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь', 'й',
+		        'b', 'c,' ,'d', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r',
+		        's', 't', 'v', 'x', 'z'
+		]
+		glasn = ['а', 'о', 'у', 'ы', 'э', 'е', 'ё', 'и', 'ю', 'я',
+		         'a', 'e', 'i', 'o', 'u', 'y'
+		]
 
-        sogl = ['б', 'в', 'г', 'д', 'ж', 'з', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ',
-                'ь', 'й']
-        glasn = ['а', 'о', 'у', 'ы', 'э', 'е', 'ё', 'и', 'ю', 'я']
+		for i in business :
 
-        for i in business:
+			if i in d :  # считаем все слово
+				t += d[i]
 
-            if i in d:  # считаем все слово
-                t += d[i]
+				if i in sogl :
+					osnova += d[i]  # считаем основу
 
-                if i in sogl:
-                    osnova += d[i]  # считаем основу
+					if osnova > 22 :  # проверяем чтобы было меньше 22
+						osnova = osnova - int(osnova / 22) * 22
 
-                    if osnova > 22:  # проверяем чтобы было меньше 22
-                        osnova = osnova - int(osnova / 22) * 22
+				if i in glasn :  # считаем жертву
+					jertva += d[i]
+					if jertva > 22 :  # проверяем чтобы было меньше 22
+						jertva = jertva - int(jertva / 22) * 22
 
-                if i in glasn:  # считаем жертву
-                    jertva += d[i]
-                    if jertva > 22:  # проверяем чтобы было меньше 22
-                        jertva = jertva - int(jertva / 22) * 22
+		# считаем цель
+		if t > 22 :  # проверяем чтобы было меньше 22
+			cel = t - int(t / 22) * 22
 
-        # считаем цель
-        if t > 22:  # проверяем чтобы было меньше 22
-            cel = t - int(t / 22) * 22
+		else :
+			cel = t
 
-        else:
-            cel = t
+		# проверяем чтобы было не равно 0
+		if cel == 0 :
+			cel = 22
 
-        # проверяем чтобы было не равно 0
-        if cel == 0:
-            cel = 22
+		if osnova == 0 :
+			osnova = 22
 
-        if osnova == 0:
-            osnova = 22
+		if jertva == 0 :
+			jertva = 22
 
-        if jertva == 0:
-            jertva = 22
+		# расчет кармичности
 
-        # расчет кармичности
+		if birth_day != '' and birth_month != '' and birth_year != '' :
 
+			birth_day = int(birth_day)
+			birth_month = int(birth_month)
 
-        if birth_day != '' and birth_month != '' and birth_year != '':
+			if (birth_day >= 14) and (birth_day <= 22) :
 
-
-            birth_day = int(birth_day)
-            birth_month = int(birth_month)
-
-            if (birth_day >= 14) and (birth_day <= 22) :
-
-                Karma1 = birth_day - birth_month
-                Karma2 = birth_day
-
-
-            else :
-                dt = birth_day
-                if (birth_day > 22) :
-                    dt = birth_day - 22
-
-                Karma1 = abs(dt - birth_month)
-
-                if Karma1 == 0 :
-                    Karma1 = 22
-                Karma2 = 0
-
-                if cel == Karma1 or cel == Karma2 :
-                    karma_txt = 'НАЗВАНИЕ КАРМИЧНО'
-                if osnova == Karma1 or osnova == Karma2 :
-                    karma_txt = 'НАЗВАНИЕ КАРМИЧНО'
-                if jertva == Karma1 or jertva == Karma2 :
-                    karma_txt = 'НАЗВАНИЕ КАРМИЧНО'
-
-            arkan_cel = Arkans.objects.get(arkan_number=cel)
-            cel_txt = arkan_cel.cel
-            arkan_osnova = Arkans.objects.get(arkan_number=osnova)
-            osnova_txt = arkan_osnova.osnova
-            arkan_jertva = Arkans.objects.get(arkan_number=jertva)
-            jertva_txt = arkan_jertva.jertva
-
-            context = {
-                'form' : userform, 'business' : business,
-                'cel' : cel, 'osnova' : osnova, 'jertva' : jertva, 'cel_txt' : cel_txt,
-                'osnova_txt' : osnova_txt, 'jertva_txt' : jertva_txt, 'birth_day' : birth_day,
-                'birth_month' : birth_month, 'birth_year' : birth_year, 'karma_txt' : karma_txt
-            }
-            return render(request, "business_name.html", context)
-
-        else:
-
-            arkan_cel = Arkans.objects.get(arkan_number=cel)
-            cel_txt = arkan_cel.cel
-            arkan_osnova = Arkans.objects.get(arkan_number=osnova)
-            osnova_txt = arkan_osnova.osnova
-            arkan_jertva = Arkans.objects.get(arkan_number=jertva)
-            jertva_txt = arkan_jertva.jertva
-
-            context = {
-                'form' : userform, 'business' : business,
-                'cel' : cel, 'osnova' : osnova, 'jertva' : jertva, 'cel_txt' : cel_txt,
-                'osnova_txt' : osnova_txt, 'jertva_txt' : jertva_txt, 'birth_day' : birth_day,
-                'birth_month' : birth_month, 'birth_year' : birth_year, 'karma_txt' : karma_txt
-            }
-            return render(request, "business_name.html", context)
+				Karma1 = birth_day - birth_month
+				Karma2 = birth_day
 
 
+			else :
+				dt = birth_day
+				if (birth_day > 22) :
+					dt = birth_day - 22
+
+				Karma1 = abs(dt - birth_month)
+
+				if Karma1 == 0 :
+					Karma1 = 22
+				Karma2 = 0
+
+				if cel == Karma1 or cel == Karma2 :
+					karma_txt = 'НАЗВАНИЕ КАРМИЧНО'
+				if osnova == Karma1 or osnova == Karma2 :
+					karma_txt = 'НАЗВАНИЕ КАРМИЧНО'
+				if jertva == Karma1 or jertva == Karma2 :
+					karma_txt = 'НАЗВАНИЕ КАРМИЧНО'
+
+			arkan_cel = Arkans.objects.get(arkan_number=cel)
+			cel_txt = arkan_cel.cel
+			arkan_osnova = Arkans.objects.get(arkan_number=osnova)
+			osnova_txt = arkan_osnova.osnova
+			arkan_jertva = Arkans.objects.get(arkan_number=jertva)
+			jertva_txt = arkan_jertva.jertva
+
+			context = {
+				'form' : userform, 'business' : business,
+				'cel' : cel, 'osnova' : osnova, 'jertva' : jertva, 'cel_txt' : cel_txt,
+				'osnova_txt' : osnova_txt, 'jertva_txt' : jertva_txt, 'birth_day' : birth_day,
+				'birth_month' : birth_month, 'birth_year' : birth_year, 'karma_txt' : karma_txt
+			}
+			return render(request, "business_name.html", context)
+
+		else :
+
+			arkan_cel = Arkans.objects.get(arkan_number=cel)
+			cel_txt = arkan_cel.cel
+			arkan_osnova = Arkans.objects.get(arkan_number=osnova)
+			osnova_txt = arkan_osnova.osnova
+			arkan_jertva = Arkans.objects.get(arkan_number=jertva)
+			jertva_txt = arkan_jertva.jertva
 
 
-    else:
-        form = BusinessForm()
-        return render(request, "business_name.html", {'form':form})
+			context = {
+				'form' : userform, 'business' : business,
+				'cel' : cel, 'osnova' : osnova, 'jertva' : jertva, 'cel_txt' : cel_txt,
+				'osnova_txt' : osnova_txt, 'jertva_txt' : jertva_txt, 'birth_day' : birth_day,
+				'birth_month' : birth_month, 'birth_year' : birth_year, 'karma_txt' : karma_txt,
+
+			}
+			return render(request, "business_name.html", context)
 
 
+
+	else :
+		form = BusinessForm()
+		return render(request, "business_name.html", {'form' : form})
